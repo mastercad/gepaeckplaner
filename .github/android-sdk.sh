fb97c3a8aaa3bfa10d76686509f123ac495fdd4d
MasterCAD <andreas.kempe@byte-artist.de>
2021-07-31 21:30:22 +0200

    - add debug messages to android-sdk.sh

--- .github/android-sdk.sh
+++ .github/android-sdk.sh
@@ -7,11 +7,15 @@ set -e
 echo "Setting up Actions Android SDK"
 
 echo "Installing sdkmanager"
+echo "Download https://dl.google.com/android/repository/commandlinetools-mac-${ANDROID_SDK_TOOLS}.zip"
 wget --quiet --output-document=android-sdk.zip https://dl.google.com/android/repository/commandlinetools-mac-${ANDROID_SDK_TOOLS}.zip
 # unpacking into cmdline-tools/ because of https://issuetracker.google.com/issues/150942306
+echo "Mkdir $ANDROID_HOME/cmdline-tools/"
 mkdir -p $ANDROID_HOME/cmdline-tools/
+echo "unzip files"
 unzip -o android-sdk.zip -d $ANDROID_HOME/cmdline-tools
 # rename folder to latest to match Android Studio/sdkmanager behavior
+echo "move folder"
 mv $ANDROID_HOME/cmdline-tools/tools $ANDROID_HOME/cmdline-tools/latest
 
 echo "Installing required Android tools"
